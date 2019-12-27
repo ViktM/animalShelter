18b29388a0e9b9f3dbc37bbb2a8da7d1280916df
laura-davis <laura.davis@sky.uk>
2019-12-27 11:32:06 +0000

    Updated Footer and validation added to classes | LD

--- Models/User.cs
+++ Models/User.cs
@@ -8,10 +8,14 @@ namespace animalShelter.Models
         public int ID { get; set; }
 
         [Required]
+        [StringLength(20, MinimumLength=2,ErrorMessage = "First Name must be between 2 and 20 characters.")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [StringLength(20, MinimumLength=2, ErrorMessage = "Last Name must be between 2 and 20 characters.")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
         
