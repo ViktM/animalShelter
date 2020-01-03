a840d3bd67926932e982512c9b49ed7f716ed62c
Lakitu Team <Lakitu@sky.uk>
2020-01-03 10:52:44 +0000

    update payment nav after successful payment. update cats for pic upload and to pass DOB

--- Pages/Cats/Create.cshtml.cs
+++ Pages/Cats/Create.cshtml.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Threading.Tasks;
 using animalShelter.Data;
 using animalShelter.Models;
@@ -24,12 +25,32 @@ namespace animalShelter.Pages.Cats
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid) return Page();
+            var emptyCat = new Cat();
 
-            _context.Cats.Add(Cat);
+            if (Cat.MainImage != null)
+            {
+                var path = Path.Combine(
+                    Directory.GetCurrentDirectory(), "wwwroot/Uploads",
+                    Cat.MainImage.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await Cat.MainImage.CopyToAsync(stream);
+                    emptyCat.MainImagePath = Cat.MainImage.FileName;
+                }
+            }
+
+            if (await TryUpdateModelAsync(emptyCat, "cat",
+                d => d.Name, d => d.Dob, d => d.Breed, d => d.Sex,
+                d => d.Summary))
+
+                if (!ModelState.IsValid)
+                    return Page();
+
+            _context.Cats.Add(emptyCat);
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("../Administration/Index");
         }
     }
 }
